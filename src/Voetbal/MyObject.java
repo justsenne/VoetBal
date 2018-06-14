1e471f3ba85676e125fec338ced94fb8b120b8d5
justsenne <senne1320@gmail.com>
2018-06-14 09:40:41 +0200

    kowala update

--- src/Voetbal/MyObject.java
+++ src/Voetbal/MyObject.java
@@ -8,49 +8,98 @@ import javax.swing.BorderFactory;
 import javax.swing.JLabel;
 import javax.swing.Timer;
 
+
 public class MyObject extends JLabel {
 
-	Timer timer;
-	int id;
+    private int doelLocatie = 3;
+    //int[] XY;
+  private  int[] doelXY;
+   private int[] diffXY;
+    int x;
+    int y;
+    Timer timer;
+    int id;
+    int[] LocationSteps;
 
-	public MyObject(int id) {
+    public MyObject(int id) {
 
-		super(String.valueOf(id));
+        super(String.valueOf(id));
 
-		this.id = id;
+        this.id = id;
 
-		this.setBorder(BorderFactory.createLineBorder(Color.black));
-		this.setHorizontalAlignment(JLabel.CENTER);
-		this.setOpaque(true);
-		this.setBackground(Color.WHITE);
-		this.setBounds(0, 600, 50, 50);
+        this.setBorder(BorderFactory.createLineBorder(Color.black));
+        this.setHorizontalAlignment(JLabel.CENTER);
+        this.setOpaque(true);
+        this.setBackground(Color.WHITE);
+        this.setBounds(0, 600, 5, 5);
 
-		timer = new Timer(10, new MoveHandler());
-		timer.start();
-	}
+        setXY();
+        timer = new Timer(10, new MoveHandler());
+        timer.start();
+    }
 
-	private void moveDown() {
-		//System.out.println("...move down ("+id+")");
-		this.setBounds(getX() + 1, getY() - 1, 50, 50);
+    private void moveDown() {
+        //System.out.println("...move down ("+id+")");
+        //int[] Location = getXY();
+        System.out.println(diffXY[0] + " " + diffXY[1]);
+        System.out.println(doelXY);
 
-		/* if (this.getY() > 100) {
-			System.out.println("Te laag > verwijder het object");
+        this.setBounds(this.getX()+ diffXY[0], this.getY() + diffXY[1], 50, 50);
+
+		 if (this.getY() < doelXY[1]) {
+			System.out.println("Te hoog > verwijder het object");
 			timer.stop();
 			((MyPanel) this.getParent()).removeObject(this);
-		} */
-	}
-
-	class MoveHandler implements ActionListener {
-
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			moveDown();
 		}
-	}
+		// nog voor x doen!
+    }
 
-	@Override
-	public String toString() {
-		return "MyObject " + id;
-	}
+    class MoveHandler implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            moveDown();
+        }
+    }
+
+
+    public void setXY() {
+        diffXY = new int[2];
+        doelXY = new int[2];
+
+        switch (doelLocatie) {
+            case 1:
+                x = 1;
+                y = -1;
+            break;
+
+            case 2:
+                x = 2;
+                y = -2;
+            break;
+
+            case 3:
+
+                //int eindX = 800;
+              // int eindY = 400;
+               doelXY[0] = 800;
+               doelXY[1] = 200;
+
+                x = 4;
+                y = -4;
+            break;
+        }
+       // XY[0] = x;
+       // XY[1] = y;
+        diffXY[0] = (doelXY[0] - this.getX())/100;
+        diffXY[1] = (doelXY[1] - this.getY())/100;
+       // return XY;
+    }
+
+
+    @Override
+    public String toString() {
+        return "MyObject " + id;
+    }
 
 }
