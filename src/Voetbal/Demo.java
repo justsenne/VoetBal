06425b80e67434bea9ef32a94c0333ec0e26a6ae
casperSteinebach <39789936+casperSteinebach@users.noreply.github.com>
2018-06-14 09:26:53 +0200

    Keeper rng toegevoegd

---
+++ src/Voetbal/Demo.java
@@ -0,0 +1,21 @@
+package Voetbal;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class Demo {
+	
+	public static void main(String[] args) {
+		JFrame myFrame = new JFrame();
+		myFrame.setSize(600, 600);
+		myFrame.setContentPane(new MyPanel());
+		myFrame.setVisible(true);
+		myFrame.setLayout(null);
+		
+	}
+}
---
+++ src/Voetbal/MyPanel.java
@@ -0,0 +1,60 @@
+package Voetbal;
+
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+public class MyPanel extends JPanel {
+
+	MyObject car;
+	Timer timer;
+	int counter;
+
+	Image background;
+	public MyPanel() {
+		try {
+			background = ImageIO.read(new File("images/Voetbal-goal.jpg"));
+		} catch (IOException e) {
+			System.out.println("Fout bij het laden van de achtergrond afbeelding. " + e.getMessage());
+		}
+
+		this.setLayout(null);
+
+		timer = new Timer(2000, new AddCarTimerHandler());
+		timer.start();
+	}
+
+	public void removeObject(MyObject obj) {
+		System.out.println("Remove obj: " + obj);
+		// Verwijder het object van het JPanel (tegenovergestelde van add())
+		this.remove(obj);
+		// Controleer het aantal objecten
+		System.out.println("Aantal objecten in het panel: " + this.getComponentCount());
+		
+	}
+
+	class AddCarTimerHandler implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			add(new MyObject(++counter));
+			System.out.println("Komt-ie! ("+counter+")");
+
+
+		}  
+	}
+
+	@Override
+	protected void paintComponent(Graphics g) {
+		// Teken de achtergrond afbeelding
+		g.drawImage(background, 0, 0, this);
+	}
+
+}
